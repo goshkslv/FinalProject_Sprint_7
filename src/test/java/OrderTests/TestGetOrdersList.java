66130296b25593ce118009fe3663da634b6de12a
goshkslv <goshkslv@gmail.com>
2023-08-09 02:04:56 +0400

    Add classes, tests

--- src/test/java/OrderTests/TestGetOrdersList.java
+++ src/test/java/OrderTests/TestGetOrdersList.java
@@ -1,2 +1,27 @@
-package OrderTests;public class TestGetOrdersList {
+package OrderTests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import org.junit.Before;
+import org.junit.Test;
+import ru.yandex.order.OrderAssertVoid;
+import ru.yandex.order.OrderSteps;
+
+public class TestGetOrdersList {
+    private OrderSteps orderSteps;
+    private OrderAssertVoid orderAssertVoid = new OrderAssertVoid();
+
+    @Before
+    public void setUp(){
+        orderSteps = new OrderSteps();
+    }
+
+    @DisplayName("Проверка получения списка заказов")
+    @Description("Тест на успешное получение списка заказов")
+    @Test
+    public void testGetOrdersList(){
+        ValidatableResponse response = orderSteps.getOrdersList();
+        orderAssertVoid.successGetOrdersList(response);
+    }
 }
